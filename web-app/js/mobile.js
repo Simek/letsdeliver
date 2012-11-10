06782a2842782e6aae064fbefb8f2645d1630202
Bartosz "Simek" Kaszubowski <gosimek@gmail.com>
2012-11-10 15:23:12 +0100

    quick commit

--- web-app/js/mobile.js
+++ web-app/js/mobile.js
@@ -1,17 +1,5 @@
+if (typeof jQuery !== 'undefined') {
+	(function($) {
 
-function initialize() {
-	latlng = new google.maps.LatLng(52.217645,21.015966);
-			
-	myOptions = {
-		zoom: 14,
-		center: latlng,
-		mapTypeId: google.maps.MapTypeId.ROADMAP
-	};
-			
-	var map = new google.maps.Map(document.getElementById("map"), myOptions);
-			
-	var marker = new google.maps.Marker({
-		position: latlng,
-		map: map
-	});
+	})(jQuery);
 }
